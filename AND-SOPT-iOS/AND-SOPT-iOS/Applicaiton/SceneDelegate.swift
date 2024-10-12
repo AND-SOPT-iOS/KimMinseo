3f235d615bbe4f733de5c52095a1c52874cfa1c4
kms0233 <cnblue789@naver.com>
2024-10-12 16:41:01 +0900

    [Feat] 스크롤 뷰 실습 (#3)

--- AND-SOPT-iOS/AND-SOPT-iOS/Applicaiton/SceneDelegate.swift
+++ AND-SOPT-iOS/AND-SOPT-iOS/Applicaiton/SceneDelegate.swift
@@ -21,7 +21,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
       
     self.window = UIWindow(windowScene: windowScene)
       
-    let navigationController = UINavigationController(rootViewController: ViewController())
+    let navigationController = UINavigationController(rootViewController: TossDetailViewController())
     self.window?.rootViewController = navigationController
       
     self.window?.makeKeyAndVisible()
--- AND-SOPT-iOS/AND-SOPT-iOS/Presentation/TossDetailViewController.swift
+++ AND-SOPT-iOS/AND-SOPT-iOS/Presentation/TossDetailViewController.swift
@@ -10,4 +10,109 @@ import UIKit
 import SnapKit
 import Then
 
+class TossDetailViewController: UIViewController {
+    
+    // MARK: - UI Properties
+    
+    private let scrollView = UIScrollView()
+    
+    private var contentView = UIView()
+    
+    private let redView = UIView()
+    
+    private let orangeView = UIView()
+    
+    private let yellowView = UIView()
+    
+    private let greenView = UIView()
+    
+    private let blueView = UIView()
+    
+    private let purpleView = UIView()
+    
+    // MARK: - Properties
+    
+    // MARK: - Lifecycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        setStyle()
+        setHierarchy()
+        setLayout()
+    }
+    
+    func setStyle() {
+        self.view.backgroundColor = .black
+        self.navigationController?.isNavigationBarHidden = true
+
+        redView.backgroundColor = .red
+        orangeView.backgroundColor = .orange
+        yellowView.backgroundColor = .yellow
+        greenView.backgroundColor = UIColor(red: 0, green: 0.5, blue: 0, alpha: 1)
+        blueView.backgroundColor = .blue
+        purpleView.backgroundColor = .purple
+    }
+    
+    func setHierarchy() {
+        self.view.addSubview(scrollView)
+        scrollView.addSubview(contentView)
+        
+        [redView, orangeView, yellowView, greenView, blueView, purpleView].forEach {
+            contentView.addSubview($0)
+        }
+    }
+    
+    func setLayout() {
+        scrollView.snp.makeConstraints {
+            $0.edges.equalToSuperview()
+        }
+        
+        contentView.snp.makeConstraints {
+            $0.edges.equalTo(scrollView)
+            $0.width.equalTo(scrollView)
+            $0.height.greaterThanOrEqualToSuperview().priority(.low)
+        }
+        
+        redView.snp.makeConstraints {
+            $0.top.equalToSuperview()
+            $0.horizontalEdges.equalToSuperview()
+            $0.height.equalTo(200)
+        }
+        
+        orangeView.snp.makeConstraints {
+            $0.top.equalTo(redView.snp.bottom)
+            $0.horizontalEdges.equalToSuperview()
+            $0.height.equalTo(200)
+        }
+        
+        yellowView.snp.makeConstraints {
+            $0.top.equalTo(orangeView.snp.bottom)
+            $0.horizontalEdges.equalToSuperview()
+            $0.height.equalTo(200)
+        }
+        
+        greenView.snp.makeConstraints {
+            $0.top.equalTo(yellowView.snp.bottom)
+            $0.horizontalEdges.equalToSuperview()
+            $0.height.equalTo(200)
+        }
+        
+        blueView.snp.makeConstraints {
+            $0.top.equalTo(greenView.snp.bottom)
+            $0.horizontalEdges.equalToSuperview()
+            $0.height.equalTo(200)
+        }
+        
+        purpleView.snp.makeConstraints {
+            $0.top.equalTo(blueView.snp.bottom)
+            $0.bottom.equalTo(contentView)
+            $0.horizontalEdges.equalToSuperview()
+            $0.height.equalTo(200)
+        }
+
+    }
+    
+    
+}
 
