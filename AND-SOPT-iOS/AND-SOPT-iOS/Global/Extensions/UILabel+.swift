6b6040fb37234ec2170fd266596fd0ab2178cffb
kms0233 <cnblue789@naver.com>
2024-11-07 11:26:04 +0900

    [Add] 특정 문자열의 색을 바꾸는 extension 추가 (#7)

--- AND-SOPT-iOS/AND-SOPT-iOS/Global/Extensions/UILabel+.swift
+++ AND-SOPT-iOS/AND-SOPT-iOS/Global/Extensions/UILabel+.swift
@@ -25,4 +25,17 @@ extension UILabel {
         self.font = UIFont(name: fontName, size: fontSize) ?? .systemFont(ofSize: fontSize)
     }
     
+    //특정 문자열의 색을 바꾸는 메소드
+    func setColorForText(_ targetText: String, with color: UIColor) {
+        guard let labelText = self.text else { return }
+        
+        let attributedString = NSMutableAttributedString(string: labelText)
+        let range = (labelText as NSString).range(of: targetText)
+        
+        if range.location != NSNotFound {
+            attributedString.addAttribute(.foregroundColor, value: color, range: range)
+            self.attributedText = attributedString
+        }
+    }
+    
 }
