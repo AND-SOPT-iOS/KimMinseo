5b2d5119cbf1c09ae8727806cae0ff72c00654e6
kms0233 <cnblue789@naver.com>
2024-11-07 10:47:02 +0900

    [Feat] 회원가입 유효성 검사 로직 구현 (#7)

--- AND-SOPT-iOS/AND-SOPT-iOS/Presentation/week4/ViewControllers/SignUpViewController.swift
+++ AND-SOPT-iOS/AND-SOPT-iOS/Presentation/week4/ViewControllers/SignUpViewController.swift
@@ -23,9 +23,9 @@ class SignUpViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        getUserToken()
         setHierarchy()
         setLayout()
+        setActions()
     }
     
     
@@ -34,10 +34,63 @@ class SignUpViewController: UIViewController {
     }
     
     func setLayout() {
+        self.navigationItem.hidesBackButton = true
+
         signUpView.snp.makeConstraints {
             $0.edges.equalToSuperview()
         }
+        
+    }
+    
+    func setActions() {
+        signUpView.signUpButton.addTarget(self, action: #selector(signUpButtonTapped), for: .touchUpInside)
+        signUpView.passwordTextField.addTarget(self, action: #selector(textFieldsDidChange), for: .editingChanged)
+        signUpView.passwordCheckTextField.addTarget(self, action: #selector(textFieldsDidChange), for: .editingChanged)
+        signUpView.nameTextField.addTarget(self, action: #selector(textFieldsDidChange), for: .editingChanged) // 추가
+        signUpView.hobbyTextField.addTarget(self, action: #selector(textFieldsDidChange), for: .editingChanged)
+    }
+    
+    @objc
+    func textFieldsDidChange() {
+        let isPasswordMatching = signUpView.passwordTextField.text == signUpView.passwordCheckTextField.text
+        
+        if isPasswordMatching {
+            signUpView.passwordMessageLabel.text = "비밀번호가 일치합니다!"
+            signUpView.passwordMessageLabel.textColor = .alertBlue
+        } else {
+            signUpView.passwordMessageLabel.text = "비밀번호가 일치하지 않습니다ㅠㅠ"
+            signUpView.passwordMessageLabel.textColor = .alertRed
+        }
+        
+        updateSignUpButtonState(isPasswordMatching: isPasswordMatching)
+    }
+    
+    @objc
+    func signUpButtonTapped() {
+        guard let username = signUpView.nameTextField.text,
+              let password = signUpView.passwordTextField.text,
+              let hobby = signUpView.hobbyTextField.text else { return }
+        
+        
+    }
+    
+}
 
+extension SignUpViewController {
+    
+    func updateSignUpButtonState(isPasswordMatching: Bool) {
+        let isUsernameFilled = !(signUpView.nameTextField.text ?? "").isEmpty
+        let isPasswordFilled = !(signUpView.passwordTextField.text ?? "").isEmpty
+        let isPasswordCheckFilled = !(signUpView.passwordCheckTextField.text ?? "").isEmpty
+        let isHobbyFilled = !(signUpView.hobbyTextField.text ?? "").isEmpty
+        
+        let isButtonEnabled = isUsernameFilled && isPasswordFilled && isPasswordCheckFilled && isHobbyFilled && isPasswordMatching
+        
+        signUpView.signUpButton.isEnabled = isButtonEnabled
+        signUpView.signUpButton.backgroundColor = isButtonEnabled ? .pink : .medgray
+    }
+    
+    
     }
 }
 
