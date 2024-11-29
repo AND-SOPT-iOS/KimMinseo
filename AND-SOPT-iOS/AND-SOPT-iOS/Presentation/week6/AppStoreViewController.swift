38f9dddd2b371ea3aa36899ea14a232224811e4b
kms0233 <cnblue789@naver.com>
2024-11-30 01:28:02 +0900

    [Feat] 네비바 제거 (#8)

--- AND-SOPT-iOS/AND-SOPT-iOS/Presentation/week6/AppStoreViewController.swift
+++ AND-SOPT-iOS/AND-SOPT-iOS/Presentation/week6/AppStoreViewController.swift
@@ -8,30 +8,43 @@
 import UIKit
 import SwiftUI
 
+import SnapKit
+import Then
+
+import UIKit
+import SwiftUI
+
+import SnapKit
+import Then
+
 class AppStoreViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .white
         
-        // SwiftUI 뷰를 생성
         let appStoreView = AppStoreView()
         
-        // UIHostingController로 SwiftUI 화면을 UIKit에 통합
         let hostingController = UIHostingController(rootView: appStoreView)
         
-        // SwiftUI 화면을 현재 뷰 컨트롤러에 추가
         addChild(hostingController)
         view.addSubview(hostingController.view)
         
-        // 레이아웃 설정
         hostingController.view.translatesAutoresizingMaskIntoConstraints = false
-        NSLayoutConstraint.activate([
-            hostingController.view.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
-            hostingController.view.leadingAnchor.constraint(equalTo: view.leadingAnchor),
-            hostingController.view.trailingAnchor.constraint(equalTo: view.trailingAnchor),
-            hostingController.view.bottomAnchor.constraint(equalTo: view.bottomAnchor)
-        ])
+        
+        hostingController.view.snp.makeConstraints {
+            $0.edges.equalToSuperview()
+        }
         
         hostingController.didMove(toParent: self)
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        navigationController?.setNavigationBarHidden(true, animated: animated)
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        navigationController?.setNavigationBarHidden(false, animated: animated)
     }
 }
